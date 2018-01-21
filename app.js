e6ac89d481a5cdc7794661161965733794557cc0
Uri Chandler <uri@bancor.network>
2018-01-21 18:11:28 +0200

    added loggers

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('work in progress');
+const colors    = require('colors');
+const cliBanner = require('cli-banner');
+
+const colorTypesMap = {
+    info : 'white',
+    warn : 'yellow',
+    error: 'red',
+    done : 'green'
+};
+
+module.exports = class Toolog {
+    constructor(toolName){
+        this._logPrefix = '['.white + toolName.blue + ']: '.white;
+    }
+
+    _log(type, msg, ...args){
+        console.log(this._logPrefix + msg[colorTypesMap[type]], ...args);
+    }
+
+    info(msg, ...args) {
+        this._log('info', msg, ...args);
+    }
+
+    warn(msg, ...args) {
+        this._log('warn', msg, ...args);
+    }
+
+    error(msg, ...args) {
+        this._log('error', msg, ...args);
+    }
+
+    done(msg, ...args) {
+        this._log('done', msg, ...args);
+    }
+
+    banner(msg){
+        console.log(cliBanner(msg, {borderColor: 'blue'}));
+    }
+};
